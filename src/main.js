e1e491bd5d35a4cc34fe2f6f379c3d5183fe0eaf
danilT9 <daniltrotsko9@gmail.com>
2025-10-21 19:58:04 +0300

    main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("work")
+import dishesListData from "../src/data/dishes.json";
+import dishesTemplate from "../src/templates/dishes.hbs";
+
+const dishesList = dishesTemplate(dishesListData);
+
+const area = document.querySelector(".area");
+area.innerHTML = dishesList;
